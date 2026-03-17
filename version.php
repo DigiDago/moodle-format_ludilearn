3a17664abf6cb59dabae347cc35ccbf3cefe89f4
j.kesraoui <j.kesraoui@pimenko.com>
2026-03-17 06:05:06 +0100

    [PIMDEV-795] - Non-gamified course where the teacher switching role and the course is in automatic game element assignment mode (#245)

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026022011;
+$plugin->version = 2026031710;
 $plugin->component = 'format_ludilearn';
-$plugin->release = '1.1.2-Moodle4.3';
+$plugin->release = '1.1.3-Moodle4.3';
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
