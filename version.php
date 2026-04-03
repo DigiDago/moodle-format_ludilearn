9bf8b45031e2c65f87e39369196848bf93a65dff
j.kesraoui <j.kesraoui@pimenko.com>
2026-04-03 04:10:30 +0200

    Fix mistake in PHP version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 20260401030;
+$plugin->version = 2026040130;
 $plugin->component = 'format_ludilearn';
 $plugin->release = '1.1.4-Moodle4.5';
 $plugin->requires = 2022041900;
